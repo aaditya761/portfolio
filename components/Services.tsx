835abfcd326d917543776e114d8dae34cc75852c
parsai <aaditya761@gmail.com>
2023-07-03 20:56:04 +0530

    routine commit

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,43 +1,63 @@
+"use client"
 import Image from "next/image";
 import variables from '../styles/component.module.scss';
-width:"5rem"
-const Services = () => {
-    return (<div>
+import {Montserrat} from "next/font/google";
+import Lottie from "lottie-react";
+import WebDevelopment from "../public/images/lottie/webdevelopment.json";
+import UXDesign from "../public/images/lottie/ux-design1.json";
+import Mobile from "../public/images/lottie/mobile.json";
 
-        <div>
-            <div>Frontend Development</div>
-            <div style={{display: "flex", marginTop:"2rem"}}>
+const montserrat = Montserrat({subsets: ['latin']})
+
+const Services = () => {
+    return (<div className={variables.services_container}>
+        <div className={variables.services_inner}>
+            <div style={{width:"50%"}}>
                 <div>
-                    <div className={variables.services_card_image_div}>
-                        <Image className={variables.services_card_image} src={require("../public/images/html.png")} alt={"html logo"}/>
-                    </div>
-                    <div>HTML</div>
-                </div>
-                <div>
-                    <div className={variables.services_card_image_div}>
-                        <Image className={variables.services_card_image} src={require("../public/images/css.png")} alt={"css logo"}/>
-                    </div>
-                    <div>CSS</div>
-                </div>
-                <div>
-                    <div className={variables.services_card_image_div}>
-                        <Image className={variables.services_card_image} src={require("../public/images/javascript.jpeg")} alt={"html logo"}/>
-                    </div>
-                    <div>Javascript</div>
-                </div>
-                <div>
-                    <div className={variables.services_card_image_div}>
-                        <Image className={variables.services_card_image} src={require("../public/images/html.png")} alt={"html logo"}/>
-                    </div>
-                    <div>HTML</div>
-                </div>
-                <div>
-                    <div className={variables.services_card_image_div}>
-                        <Image className={variables.services_card_image} src={require("../public/images/html.png")} alt={"html logo"}/>
-                    </div>
-                    <div>HTML</div>
+                    <Lottie style={{height:"25rem"}} animationData={UXDesign} />
                 </div>
             </div>
+            <div style={{width:"50%"}}>
+                <div className={`${variables.section_title} ${montserrat.className}`}>
+                    UI/UX Design
+                </div>
+                <div className={`${variables.wu_description} ${montserrat.className}`}>
+                    Proofs-of-concept, MVPs, advanced web/mobile products, enterprise solutions - you name it and our product design team will come up with the best solution for your business. Simplifying complex user experience with a strong focus on usability is what we strive for.
+                </div>
+            </div>
+        </div>
+        <div className={variables.services_inner}>
+            <div style={{width:"50%"}}>
+                <div className={`${variables.section_title} ${montserrat.className}`}>
+                    Web Development
+                </div>
+                <div className={`${variables.wu_description} ${montserrat.className}`}>
+                    From the get-go, our web developers will invest time to know your business, your niche market, and
+                    your goals to deliver highly- focused web-based products. We don’t believe in making a relationship
+                    mere transactional, we go above and beyond to build a partnership.
+                </div>
+            </div>
+            <div style={{width:"50%"}}>
+                <div>
+                    <Lottie style={{height:"25rem"}} animationData={WebDevelopment} />
+                </div>
+            </div>
+        </div>
+        <div className={variables.services_inner}>
+            <div style={{width:"50%"}}>
+                <div>
+                    <Lottie style={{height:"25rem"}} animationData={Mobile} />
+                </div>
+            </div>
+            <div style={{width:"50%"}}>
+                <div className={`${variables.section_title} ${montserrat.className}`}>
+                    Mobile Development
+                </div>
+                <div className={`${variables.wu_description} ${montserrat.className}`}>
+                    Get thorough, well-researched, and tech- oriented mobile app design & development solutions with an unmatched focus on transparency, project management and communication.
+                </div>
+            </div>
+
         </div>
     </div>)
 }
