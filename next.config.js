757d14557f447ed91db5a053593ffd00b10ec87c
parsai <aaditya761@gmail.com>
2023-07-05 16:32:03 +0530

    routine commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
+    },
+    generateEtags: true,
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3.amazonaws.com',
+                port: '',
+                pathname: '/block-developers/**',
+            },
+        ],
     },
 }
 
